ba2fdfc0eb9ed92834baa3977434f26a2e2b557f
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-10 16:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function map(numbers) {
-  return numbers.map(n => -n)
+function sayHello(name="") {
+  console.log(`Hello${name}`)
+}
+ 
+let sayHola = function(name="") {
+  console.log(`Hola${name}`)
+}
+ 
+functionUsingCallback(sayHello, sayHola, function(name="") {
+  console.log(`Ni Hao${name}`)
+}, " Gary")
+ 
+function map(en, es, zh, name){
+  en(name)
+  es(name)
+  zh(name)
 }
