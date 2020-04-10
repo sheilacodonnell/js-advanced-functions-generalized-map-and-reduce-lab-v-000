8657ec77ce8f84d1a6243789b567ef450e82b305
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-10 16:07:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(original, callBack){
+    let arr = []
+    for(let i=0; i< original.length; i++){
+        arr.push(callBack(original[i]))
+    }
+    return arr;
+ }
+
+ function reduce(original, callBack, start){ 
+    let val = (!!start) ? start : original[0]
+    let i = (!!start)? 0 : 1
+    for(; i < original.length; i++){
+        val = callBack(original[i], val)
+    }
+    return val;
+} 
